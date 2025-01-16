e50f8918a1d828f012c1dce7bfd2aa135e218432
Your Name <golamsarwar1896@gmail.com>
2025-01-16 22:33:19 +0600

    Data updated. successfully showing data in the ui from db. will work on delete and edit now

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,22 @@ import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./provider/AuthProvider.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
-        <RouterProvider router={router} />
-        <Toaster position="top-center" reverseOrder={false}></Toaster>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <Toaster position="top-center" reverseOrder={false}></Toaster>
+        </QueryClientProvider>
       </AuthProvider>
     </HelmetProvider>
   </StrictMode>
--- src/pages/dashboard/sidebar/employee/WorkSheet.jsx
+++ src/pages/dashboard/sidebar/employee/WorkSheet.jsx
@@ -1,12 +1,29 @@
 import { Dropdown } from "flowbite-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { format, parse } from "date-fns";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { Table } from "flowbite-react";
+import { MdOutlineEditNote } from "react-icons/md";
+import { AiOutlineDelete } from "react-icons/ai";
+import useAuth from "../../../../hooks/useAuth";
 
 const WorkSheet = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
   const [tasks, setTasks] = useState("");
   const [startDate, setStartDate] = useState(new Date());
+  const [workSheets, setWorkSheets] = useState([]);
+
+  //Fetching worksheet data
+  useEffect(() => {
+    fetchWorkSheet();
+  }, []);
+  const fetchWorkSheet = async () => {
+    const { data } = await axiosSecure.get(`/work-sheet/${user?.email}`);
+    setWorkSheets(data);
+  };
 
   //task onClick handler
   const handleTask = (value) => {
@@ -28,16 +45,29 @@ const WorkSheet = () => {
     console.log(formattedDate);
     const hours_worked = form.hours_worked.value;
 
-    const workSheet = {
+    const workSheetInfo = {
       task,
       hours_worked,
       formattedDate,
+      email: user?.email,
     };
-    console.log(workSheet);
+    console.log(workSheetInfo);
+
+    //post request to db
+    const result = axiosSecure.post("/work-sheet", workSheetInfo);
+    console.log(result);
+
+    // refetching
+    fetchWorkSheet();
   };
+
+  const handleUpdate = () => {
+    console.log("Updating");
+  };
+
   return (
-    <div className="p-10 ">
-      <h1 className="text-5xl text-center font-bold text-primaryColor">
+    <div className="pr-10">
+      <h1 className="text-5xl text-center font-bold text-primaryColor mt-10">
         Work <span className="text-darkMode">Sheet</span>
       </h1>
       {/* Task Menu */}
@@ -80,11 +110,48 @@ const WorkSheet = () => {
           <input
             type="submit"
             value="Submit"
-            className="btn px-7 py-2 bg-primaryColor text-white rounded-3xl "
+            className="btn px-7 py-2 bg-primaryColor text-white rounded-3xl cursor-pointer"
           ></input>
         </form>
       </div>
-      <div></div>
+      {/* Table Content */}
+      <div className="mt-10">
+        <div className="overflow-x-auto">
+          <Table>
+            <Table.Head>
+              <Table.HeadCell>Task Type</Table.HeadCell>
+              <Table.HeadCell>Hours Worked</Table.HeadCell>
+              <Table.HeadCell>Submitted</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>Edit</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {workSheets.map((workSheet) => (
+                <Table.Row
+                  key={workSheet._id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {workSheet.task}
+                  </Table.Cell>
+                  <Table.Cell>{workSheet.hours_worked}</Table.Cell>
+                  <Table.Cell>{workSheet.formattedDate}</Table.Cell>
+                  <Table.Cell>
+                    <button>
+                      <AiOutlineDelete className="text-xl" />
+                    </button>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <button onClick={handleUpdate}>
+                      <MdOutlineEditNote className="text-2xl" />
+                    </button>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 };
