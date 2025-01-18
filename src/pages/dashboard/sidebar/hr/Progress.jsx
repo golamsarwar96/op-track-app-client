05d86eb8b59f76ee6f3f0f573f914bd3c0028c1e
Your Name <golamsarwar1896@gmail.com>
2025-01-18 14:08:24 +0600

    Implemented Progress section by filtering work sheet data and get unique names so that the drop down doesn't have any duplication in names and months

--- src/pages/dashboard/sidebar/hr/Progress.jsx
+++ src/pages/dashboard/sidebar/hr/Progress.jsx
@@ -1,23 +1,95 @@
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 import { Table } from "flowbite-react";
 
 const Progress = () => {
   const axiosSecure = useAxiosSecure();
+  const [selectedEmployee, setSelectedEmployee] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("");
+
+  //fetching all data from work-sheet collection
   const { data: workSheet = [], refetch } = useQuery({
     queryKey: ["workSheet"],
     queryFn: async () => {
-      const { data } = await axiosSecure.get(`/work-sheet`);
+      const { data } = await axiosSecure.get("/work-sheet");
+      console.log(data);
       return data;
     },
   });
+
+  // Filtering work-sheet data to get specific results
+  const filteredData = workSheet.filter((workSheet) => {
+    const isEmployee =
+      selectedEmployee === "" || workSheet.name === selectedEmployee;
+
+    const isMonth =
+      selectedMonth === "" ||
+      workSheet.formattedDate.startsWith(selectedMonth + "/");
+
+    return isEmployee && isMonth;
+  });
+
+  // Unique Names for selection and map
+  const employeeNames = [];
+  for (let i = 0; i < workSheet.length; i++) {
+    if (!employeeNames.includes(workSheet[i].name)) {
+      employeeNames.push(workSheet[i].name);
+    }
+  }
+
+  // Unique months for dropdown
+  const months = [];
+  for (let i = 0; i < workSheet.length; i++) {
+    const month = workSheet[i].formattedDate.split("/")[0];
+    if (!months.includes(month)) {
+      months.push(month);
+    }
+  }
+
   return (
-    <div className="pr-10">
+    <div>
       <h1 className="text-5xl text-center font-bold text-primaryColor mt-10">
         Prog<span className="text-darkMode">ress</span>
       </h1>
-
-      {/* Table section */}
+      {/* Dropdown section */}
+      <div className="flex justify-center items-center mt-10 gap-12">
+        <div className="">
+          <div>
+            <label className="mr-2 font-medium">Select Employee:</label>
+            <select
+              value={selectedEmployee}
+              onChange={(e) => setSelectedEmployee(e.target.value)}
+              className="p-2 border rounded"
+            >
+              <option>All Employee</option>
+              {employeeNames.map((name, index) => (
+                <option key={index} value={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label className="mr-2 font-medium">Select Month:</label>
+            <select
+              value={selectedMonth}
+              onChange={(e) => setSelectedMonth(e.target.value)}
+              className="p-2 border rounded"
+            >
+              <option value="">All Months</option>
+              {months.map((month, index) => (
+                <option key={index} value={month}>
+                  {month}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      {/* Table Section */}
       <div className="mt-10">
         <div className="overflow-x-auto">
           <Table>
@@ -27,7 +99,7 @@ const Progress = () => {
               <Table.HeadCell>Submitted Date</Table.HeadCell>
             </Table.Head>
             <Table.Body className="divide-y">
-              {workSheet.map((workSheet) => (
+              {filteredData.map((workSheet) => (
                 <Table.Row
                   key={workSheet._id}
                   className="bg-white dark:border-gray-700 dark:bg-gray-800"
