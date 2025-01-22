525be16fcfa6be772e83bd779a7a2aa1b9e00a8f
Your Name <golamsarwar1896@gmail.com>
2025-01-22 18:52:32 +0600

    Final push. still fixing loading icon

--- src/pages/shared/Loading.jsx
+++ src/pages/shared/Loading.jsx
@@ -2,7 +2,7 @@ import { Spinner } from "flowbite-react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center mt-52]">
+    <div className="flex justify-center items-center mt-52">
       <Spinner className="text-4xl" aria-label="Default status example" />;
     </div>
   );
