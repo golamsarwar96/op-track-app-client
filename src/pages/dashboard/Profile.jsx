7783b2554f45269e728c07f5e538d054db6aa7fc
Your Name <golamsarwar1896@gmail.com>
2025-02-19 07:26:36 +0600

    Added the profile card for all the roles.

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -32,16 +32,34 @@ const Profile = () => {
 
   return (
     <div>
-      <h1>Profile</h1>
-      <div>
+      <h1 className="text-center text-5xl text-darkMode mt-10 font-bold">
+        <span className="text-primaryColor">Pro</span>file
+      </h1>
+      <div className="flex justify-center items-center mt-10 bg-primaryColor rounded-3xl w-[40%] mx-auto flex-col py-10">
         <img
           referrerPolicy="no-referrer"
+          className="w-[150px] h-[150px] rounded-full"
           src={
             singleUser?.role === "Employee"
               ? singleUser.image
               : singleUser.img_URL
           }
         />
+        <h1 className="text-darkMode font-bold text-xl mt-3">
+          {singleUser?.name} {singleUser?.isVerified ? "✅" : "❌"}
+        </h1>
+        <h1 className="text-darkMode font-bold text-xl mt-3">
+          Email: {singleUser?.email}
+        </h1>
+        <h1 className="text-darkMode font-bold text-xl mt-3">
+          Designation: {singleUser?.designation}
+        </h1>
+        <h1 className="text-darkMode font-bold text-xl mt-3">
+          Salary: ${singleUser?.salary}
+        </h1>
+        <h1 className="text-darkMode font-bold text-xl mt-3">
+          Bank Account No. : {singleUser?.bank_account_no}
+        </h1>
       </div>
     </div>
   );
